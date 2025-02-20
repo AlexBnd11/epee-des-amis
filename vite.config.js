7624bd684c76113529cd1e1102351db083fbdf90
Alex <alexbonniard@gmail.com>
2025-02-20 10:56:56 +0100

    fix déploiement 2

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/epee-des-amis/',  // Nom de votre repo
+  base: '/l-epee-des-amis/',  // Nom de votre repo
 })
